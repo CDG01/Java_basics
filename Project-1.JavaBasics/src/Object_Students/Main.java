1616c7c3c288d17c6d9049185d86f69a51f5e8c5
Chiara De Guglielmo <154986750+CDG01@users.noreply.github.com>
2024-02-26 18:59:32 +0100

    bug: package Object_Students doesn't work

---
+++ Project-1.JavaBasics/src/Object_Students/Main.java
@@ -0,0 +1,14 @@
+/*
+Scrivere un programma che contenga una classe che definisce uno Studente
+che abbia come proprietà nome, cognome ed identificativo.
+Per questa classe saranno necessari i metodi accessori (getters e setters) ed un costruttore.
+Nel main dichiarare un oggetto di tipo Studente.
+ */
+
+package Object_Students;
+
+public class Main {
+    Students studentSpecial = new Students("Chiara", "De Guglielmo");
+
+    System.out.println(studentSpecial.getName())
+}
---
+++ Project-1.JavaBasics/src/Object_Students/Students.java
@@ -0,0 +1,43 @@
+package Object_Students;
+
+import java.util.UUID;
+
+public class Students {
+    // ATTRIBUTI
+    private String name;
+    private String surname;
+    private String ID;
+
+    // METODI
+
+    // costruttore
+    public Students(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+        this.ID = UUID.randomUUID().toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+}
