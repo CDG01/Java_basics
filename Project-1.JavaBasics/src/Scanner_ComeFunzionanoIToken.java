38eb0e4030394146131e7285e3aca6b7cd88db62
Chiara De Guglielmo <154986750+CDG01@users.noreply.github.com>
2024-03-27 18:59:39 +0100

    comparable

---
+++ Project-1.JavaBasics/src/Scanner_ComeFunzionanoIToken.java
@@ -0,0 +1,27 @@
+/*
+come funziona il "next token" e il buffer della classe scanner?
+I token (ogni parola/numero circondato da spazi) rimangono nel buffer finché non vengono letti
+ */
+
+import java.util.Scanner;
+
+public class Scanner_ComeFunzionanoIToken {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.println("Inserisci una sequenza di numeri");
+                float num1 = scanner.nextFloat();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+                System.out.println("fine, inserisci un token:");
+                scanner.next();
+            }
+        }
+
+
+    }
+
+}
